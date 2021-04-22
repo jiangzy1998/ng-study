f56aa49c1d1e1a5d34fff71fae0243d13ee29d85
jiangzy <jiangzy1998@gmail.com>
2021-04-22 21:00:27 +0800

    jiangsu-audit svg

--- src/app/com/com-routing.module.ts
+++ src/app/com/com-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComObservableComponent } from "./com-observable/com-observable.component"
 import { ChangedetectorrefComponent } from './com-ref/changedetectorref/changedetectorref.component';
+import { SvgComponent } from './com-svg/svg/svg.component';
 
 const routes: Routes = [
   // 重定向出现 url 只有 /observable，未解决
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path:"changedetectorref",
     component:ChangedetectorrefComponent
+  },
+  {
+    path:"svg",
+    component:SvgComponent
   }
 ];
 
---
+++ src/app/com/com-svg/svg/svg.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg',
+  templateUrl: './svg.component.html',
+  styleUrls: ['./svg.component.less']
+})
+export class SvgComponent implements OnInit {
+
+  num:number[]=[17,16,17,17,16,16];
+  loc:number[]=[];
+  constructor() { 
+    this.CalculateCircle(this.num,112);
+    console.log(this.loc);
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  CalculateCircle(num:number[],circleR:number){
+    let alldeg=0;
+    this.loc=[];
+    this.loc.push(circleR-2,0);
+    for(let n in num){
+        alldeg += num[n]*3.6;
+        console.log(alldeg);
+        if(alldeg<90){
+          let x = circleR + circleR/Math.cos(alldeg*2*Math.PI/360)
+          this.loc.push(x,0);
+        }
+        else if(alldeg==90){
+          this.loc.push(2*circleR,circleR);
+        }
+        else if(alldeg>90 && alldeg<180){
+          let y = circleR + circleR/Math.cos((alldeg-90)*2*Math.PI/360)
+          this.loc.push(2*circleR,y);
+        }
+        else if(alldeg==180){
+          this.loc.push(circleR-1,2*circleR);
+        }
+        else if(alldeg>180&&alldeg<270){
+          let x = circleR - circleR/Math.cos((alldeg-180)*2*Math.PI/360)
+          this.loc.push(x,2*circleR);
+        }
+        else if(alldeg==270){
+          this.loc.push(0,circleR);
+        }
+
+        else if(alldeg>270 &&alldeg<360){
+          let y = circleR - circleR/Math.cos((alldeg-270)*2*Math.PI/360)
+          this.loc.push(0,y);
+        }
+    }
+  }
+
+}
