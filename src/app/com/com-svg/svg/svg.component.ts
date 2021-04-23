2788e2e2c24b50336e5849c5b61ea00d9daa617e
jiangzy <jiangzy1998@gmail.com>
2021-04-23 10:35:52 +0800

    qz js audit svg circle

--- src/app/com/com-svg/svg/svg.component.ts
+++ src/app/com/com-svg/svg/svg.component.ts
@@ -22,36 +22,39 @@ export class SvgComponent implements OnInit {
     let alldeg=0;
     this.loc=[];
     this.loc.push(circleR-2,0);
+    let total = eval(num.join("+"))
     for(let n in num){
-        alldeg += num[n]*3.6;
+        alldeg += num[n]*(360/total);
         console.log(alldeg);
         if(alldeg<90){
-          let x = circleR + circleR/Math.cos(alldeg*2*Math.PI/360)
+          let x = circleR + circleR*Math.tan(alldeg*2*Math.PI/360)
           this.loc.push(x,0);
         }
         else if(alldeg==90){
           this.loc.push(2*circleR,circleR);
         }
         else if(alldeg>90 && alldeg<180){
-          let y = circleR + circleR/Math.cos((alldeg-90)*2*Math.PI/360)
+          let y = circleR + circleR*Math.tan((alldeg-90)*2*Math.PI/360)
           this.loc.push(2*circleR,y);
         }
         else if(alldeg==180){
-          this.loc.push(circleR-1,2*circleR);
+          this.loc.push(circleR+1,2*circleR);
         }
         else if(alldeg>180&&alldeg<270){
-          let x = circleR - circleR/Math.cos((alldeg-180)*2*Math.PI/360)
+          let x = circleR - circleR*Math.tan((alldeg-180)*2*Math.PI/360)
           this.loc.push(x,2*circleR);
         }
         else if(alldeg==270){
           this.loc.push(0,circleR);
         }
-
         else if(alldeg>270 &&alldeg<360){
-          let y = circleR - circleR/Math.cos((alldeg-270)*2*Math.PI/360)
+          let y = circleR - circleR*Math.tan((alldeg-270)*2*Math.PI/360)
           this.loc.push(0,y);
         }
+        console.log(this.loc)
     }
+    // this.loc.pop()
+    // this.loc.pop()
   }
 
 }
